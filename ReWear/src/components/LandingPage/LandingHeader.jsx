1a9705d23e0e61ac5cfb827aa04d1ac9930473e1
Pritam <kumarroypritam2@gmail.com>
2025-07-12 16:41:55 +0530

    2nd full page push

--- ReWear/src/components/LandingPage/LandingHeader.jsx
+++ ReWear/src/components/LandingPage/LandingHeader.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LandingHeader.css";
 import SearchBar from "./SearchBar";
+import AuthModal from "./AuthModal";
+import "./LandingHeaderActions.css";
 
-const LandingHeader = () => (
-  <header className="landing-header">
-    <div className="logo">Rewear</div>
-    <SearchBar />
-    <div className="user-profile-icon">
-      <i className="fas fa-user-circle"></i>
-    </div>
-  </header>
-);
+const LandingHeader = () => {
+  const [showAuth, setShowAuth] = useState(false);
+  return (
+    <header className="landing-header">
+      <div className="logo">Rewear</div>
+      <SearchBar />
+      <div className="landing-header-actions">
+        <button className="login-btn" onClick={() => setShowAuth(true)}>
+          Login / Sign Up
+        </button>
+        <div className="user-profile-icon">
+          <i className="fas fa-user-circle"></i>
+        </div>
+      </div>
+      <AuthModal isOpen={showAuth} onClose={() => setShowAuth(false)} />
+    </header>
+  );
+};
 
 export default LandingHeader;
